6c8a8b935363f5e308674300a44fbaff934a4efe
krystiandec <49121182+krystiandec@users.noreply.github.com>
2020-10-15 14:13:27 +0200

    zadanie 2 wykonane w jednej metodzie - podejście algorytmiczne

--- src/Services/training/interviewIssues/numberPairsMaxMin/Pairs.java
+++ src/Services/training/interviewIssues/numberPairsMaxMin/Pairs.java
@@ -1,17 +1,50 @@
 package Services.training.interviewIssues.numberPairsMaxMin;
 
+import java.util.*;
+
 public class Pairs {
-private static int[]  maxMinNextNumbers(int[] array){
-    int[] maxPair = new int[2];
-    int[] minPair = new int[2];
-
-
-
-
-    return array;
-}
+    private static void maxMinNextNumbers(int[] array) {
+        Set<Integer> eachSum = new HashSet<>();
+        for (int i = 0; i < array.length - 1; i++) {
+            eachSum.add(array[i] + array[i + 1]);
+        }
+        int maxVal = eachSum.stream().max(Integer::compareTo).get();
+        int minVal = eachSum.stream().min(Integer::compareTo).get();
+        Set<List<Integer>> maxSet = new HashSet<>();
+        Set<List<Integer>> minSet = new HashSet<>();
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        map.computeIfAbsent(1, integer -> new ArrayList<>());
+        map.computeIfPresent(1, (integer, integers) ->
+                {
+                    integers.add(maxVal);
+                    return integers;
+                }
+        );
+        for (int i = 0; i < array.length - 1; i++) {
+            if (array[i] + array[i + 1] >= maxVal) {
+                List<Integer> maxList = new ArrayList<>();
+                maxList.add(array[i]);
+                maxList.add(array[i + 1]);
+                maxSet.add(maxList);
+            } else if (array[i] <= minVal && (array[i] + array[i + 1] == minVal)) {
+                List<Integer> minList = new ArrayList<>();
+                minList.add(array[i]);
+                minList.add(array[i + 1]);
+                minSet.add(minList);
+            }
+        }
+        System.out.println("Pary kolejnych liczb dających największą sumę:");
+        for (List<Integer> i : maxSet) {
+            System.out.println(i.toString());
+        }
+        System.out.println("Pary kolejnych liczb dających najmniejszą sumę:");
+        for (List<Integer> i : minSet) {
+            System.out.println(i.toString());
+        }
+    }
 
     public static void main(String[] args) {
-
+        int[] income = {-5, 0, 10, -5, 0, 5, 5};
+        maxMinNextNumbers(income);
     }
 }
