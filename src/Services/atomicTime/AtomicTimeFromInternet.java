7786a1cd77baff86fe9e5211f3a0da9f67771948
krystiandec <49121182+krystiandec@users.noreply.github.com>
2020-11-02 11:53:27 +0100

    zrobiono pobieranie czasu z serwera

---
+++ src/Services/atomicTime/AtomicTimeFromInternet.java
@@ -0,0 +1,61 @@
+package Services.atomicTime;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.Socket;
+import java.time.OffsetDateTime;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.TimeZone;
+import java.util.Timer;
+
+public class AtomicTimeFromInternet {
+    /*więcej informacji na temat pobierania czasu z internetów:
+    *https://tf.nist.gov/tf-cgi/servers.cgi
+    *https://www.rgagnon.com/javadetails/java-0589.html  */
+    public static final String ATOMICTIME_SERVER = "129.6.15.30";
+    public static final int ATOMICTIME_PORT = 13;
+
+    public final static GregorianCalendar getAtomicTime() throws IOException {
+        BufferedReader in = null;
+        Socket conn = null;
+        try {
+            conn = new Socket(ATOMICTIME_SERVER, ATOMICTIME_PORT);
+            in = new BufferedReader
+                    (new InputStreamReader(conn.getInputStream()));
+            String atomicTime;
+            while (true) {
+                if ((atomicTime = in.readLine()).contains("*")) {
+                    break;
+                }
+            }
+/*            TimeZone tz = TimeZone.getTimeZone("EST"); // or .getDefault()
+            int gmt = (tz.getRawOffset() + tz.getDSTSavings()) / 3600000;*/
+            GregorianCalendar calendar = new GregorianCalendar();
+            String[] fields = atomicTime.split(" ");
+
+            String[] time = fields[2].split(":");
+            calendar.set(Calendar.HOUR_OF_DAY, Integer.parseInt(time[0]));
+            calendar.set(Calendar.MINUTE, Integer.parseInt(time[1]));
+            calendar.set(Calendar.SECOND, Integer.parseInt(time[2]));
+            return calendar;
+
+        } catch (IOException e) {
+            e.getMessage();
+            throw e;
+        } finally {
+            if (in != null) {in.close();}
+            if (conn != null) {conn.close();}
+        }
+    }
+
+    public static void main(String[] args) {
+        try {
+            System.out.println(getAtomicTime().getTime());
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
--- src/Services/training/interviewIssues/stringConstantPool/StringTask.java
+++ src/Services/training/interviewIssues/stringConstantPool/StringTask.java
@@ -2,9 +2,9 @@ package Services.training.interviewIssues.stringConstantPool;
 
 public class StringTask {
     public static void main(String[] args) {
-        String string1 = "Hello".concat("World");           // Linia-1
-//        String string2 = new String("HelloWorld");          // Linia-2
-        String string3 = string1.intern();                  // Linia-3
-        System.out.println(string1 == string3);             // Linia-4
+        String string1 = "Hello".concat("World");                              // Linia-1
+        String string2 = new String("HelloWorld");          // Linia-2
+        String string3 = string1.intern();                                           // Linia-3
+        System.out.println(string1 == string3);                                 // Linia-4
     }
 }
